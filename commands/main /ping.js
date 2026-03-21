13ab40b39d65dbfdf91de15953ef264987b84755
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <wilkermusic03@gmail.com>
2026-03-21 00:33:04 -0400

    Update ping.js

--- commands/main /ping.js
+++ commands/main /ping.js
@@ -5,6 +5,6 @@ export default {
     const start = Date.now()
     const sent = await client.sendMessage(m.chat, { text: '`❏ ¡Pong!`' + `\n> *${global.db.data.settings[client.user.id.split(':')[0] + "@s.whatsapp.net"].namebot}*`}, { quoted: m })
     const latency = Date.now() - start
-    await client.sendMessage(m.chat, { text: `✿ *Pong!*\n> Tiempo ⴵ ${latency}ms`, edit: sent.key }, { quoted: m })
+    await client.sendMessage(m.chat, { text: `✿ *Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms`, edit: sent.key }, { quoted: m })
   },
 };
