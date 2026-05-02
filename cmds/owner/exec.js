20590513523722af9d70adc2ebafcaf57ca892c2
todostodos676-collab <todostodos676@gmail.com>
2026-05-01 18:58:03 -0600

    Actualizar exec.js

--- cmds/owner/exec.js
+++ cmds/owner/exec.js
@@ -6,36 +6,50 @@ import { createRequire } from 'module'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
+const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor
+
 export default {
   command: ['ex', 'e'],
   category: 'owner',
   isOwner: true,
   run: async (client, m, args, usedPrefix, command, text) => {
-  if (!text.trim()) {
-   return client.reply(m.chat, '《✧》 Debes escribir un comando a ejecutar.', m)
-  }
+    if (!text || !text.trim()) {
+      return client.reply(m.chat, '《✧》 Debes escribir un comando a ejecutar.', m)
+    }
+
     let _text = (command === 'e' ? 'return ' : '') + text
-    let old = m.exp * 1
+    let old = m.exp || 0
     let _return, _syntax = ''
+
     try {
-      await m.react('🕒')
+      if (m.react) await m.react('🕒')
+
       let i = 15
       let f = { exports: {} }
-      let exec = new (async () => {}).constructor('print', 'm', 'client', 'require', 'Array', 'process', 'args', 'module', 'exports', 'argument', _text)
-       _return = await exec.call(client, (...args) => {
+
+      let exec = new AsyncFunction(
+        'print', 'm', 'client', 'require', 'Array', 'process', 'args', 'module', 'exports', 'argument',
+        _text
+      )
+
+      _return = await exec.call(client, (...args) => {
         if (--i < 1) return
         return client.reply(m.chat, format(...args), m)
       }, m, client, require, Array, process, args, f, f.exports, [client])
-      await m.react('✔️')
+
+      if (m.react) await m.react('✔️')
+
     } catch (e) {
       let err = syntaxerror(_text, 'Execution Function', {
         allowReturnOutsideFunction: true,
         allowAwaitOutsideFunction: true,
         sourceType: 'module'
       })
+
       if (err) _syntax = '```' + err + '```\n\n'
       _return = e
-      await m.react('✖️')
+
+      if (m.react) await m.react('✖️')
     } finally {
       client.reply(m.chat, _syntax + format(_return), m)
       m.exp = old
