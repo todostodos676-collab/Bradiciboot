8cf1426312cb2a616ed4b11f341a4f27c33dcdab
todostodos676-collab <todostodos676@gmail.com>
2026-05-01 19:28:40 -0600

    Actualizar events.js

--- cmds/events.js
+++ cmds/events.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch'
-let WAMessageStubType = (await import('@whiskeysockets/baileys')).default
 import chalk from 'chalk'
 
 export default async (client, m) => {
@@ -14,8 +12,8 @@ export default async (client, m) => {
       const isSelf = global.db.data.settings[botId]?.self ?? false
       if (isSelf) return
       for (const p of anu.participants) {
-        const jid = p.phoneNumber
-        const phone = p.phoneNumber?.split('@')[0] || jid.split('@')[0]
+        const jid = p
+        const phone = jid.split('@')[0]
         const pp = await client.profilePictureUrl(jid, 'image').catch(_ => 'https://cdn.yuki-wabot.my.id/files/2PVh.jpeg')       
         const mensajes = { add: chat.sWelcome ? `\n┊➤ ${chat.sWelcome.replace(/{usuario}/g, `@${phone}`).replace(/{grupo}/g, `*${metadata.subject}*`).replace(/{desc}/g, metadata?.desc || '✿ Sin Desc ✿')}` : '', remove: chat.sGoodbye ? `\n┊➤ ${chat.sGoodbye.replace(/{usuario}/g, `@${phone}`).replace(/{grupo}/g, `*${metadata.subject}*`).replace(/{desc}/g, metadata?.desc || '✿ Sin Desc ✿')}` : '', leave: chat.sGoodbye ? `\n┊➤ ${chat.sGoodbye.replace(/{usuario}/g, `@${phone}`).replace(/{grupo}/g, `*${metadata.subject}*`).replace(/{desc}/g, metadata?.desc || '✿ Sin Desc ✿')}` : '' }
         const fakeContext = {
@@ -51,7 +49,7 @@ export default async (client, m) => {
 ┊➤ *Ahora somos ${memberCount} miembros.* ${mensajes[anu.action]}
 ┊ ︿︿︿︿︿︿︿︿︿︿︿
 ╰─────────────────╯`
-         await client.sendMessage(anu.id, { image: { url: pp }, caption, ...fakeContext })     
+          await client.sendMessage(anu.id, { image: { url: pp }, caption, ...fakeContext })     
         }
         if ((anu.action === 'remove' || anu.action === 'leave') && chat?.goodbye && (!primaryBotId || primaryBotId === botId)) {
           const caption = `╭┈──̇─̇─̇────̇─̇─̇──◯◝
@@ -79,37 +77,38 @@ export default async (client, m) => {
       console.log(chalk.gray(`[ BOT  ]  → ${err}`))
     }
   })
+
   client.ev.on('messages.upsert', async ({ messages }) => {
-  const m = messages[0]
-  if (!m.messageStubType) return
-  const id = m.key.remoteJid
-  const chat = global.db.data.chats[id]
-  const botId = client.user.id.split(':')[0] + '@s.whatsapp.net'
-  const primaryBotId = chat?.primaryBot
-  if (!chat?.alerts || (primaryBotId && primaryBotId !== botId)) return
-  const isSelf = global.db.data.settings[botId]?.self ?? false
-  if (isSelf) return
-  const actor = m.key?.participant || m.participant || m.key?.remoteJid
-  const phone = actor.split('@')[0]
-  const groupMetadata = await client.groupMetadata(id).catch(() => null)
-  const groupAdmins = groupMetadata?.participants.filter(p => (p.admin === 'admin' || p.admin === 'superadmin')) || []
-  if (m.messageStubType == 21) {
-    await client.sendMessage(id, { text: `「✎」 @${phone} cambió el nombre del grupo a *${m.messageStubParameters[0]}*`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
-  }
-  if (m.messageStubType == 22) {
-    await client.sendMessage(id, { text: `「✎」 @${phone} cambió el icono del grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
-  }
-  if (m.messageStubType == 23) {
-    await client.sendMessage(id, { text: `「✎」 @${phone} restableció el enlace del grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
-  }
-  if (m.messageStubType == 24) {
-    await client.sendMessage(id, { text: `「✎」 @${phone} cambió la descripción del grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
-  }
-  if (m.messageStubType == 25) {
-    await client.sendMessage(id, { text: `「✎」 @${phone} cambió los ajustes del grupo para permitir que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
-  }
-  if (m.messageStubType == 26) {
-    await client.sendMessage(id, { text: `「✎」 @${phone} cambió los ajustes del grupo para permitir que ${m.messageStubParameters[0] === 'on' ? 'solo los administradores puedan enviar mensajes al grupo.' : 'todos los miembros puedan enviar mensajes al grupo.'}`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
-  }
-})
+    const m = messages[0]
+    if (!m.messageStubType) return
+    const id = m.key.remoteJid
+    const chat = global.db.data.chats[id]
+    const botId = client.user.id.split(':')[0] + '@s.whatsapp.net'
+    const primaryBotId = chat?.primaryBot
+    if (!chat?.alerts || (primaryBotId && primaryBotId !== botId)) return
+    const isSelf = global.db.data.settings[botId]?.self ?? false
+    if (isSelf) return
+    const actor = m.key?.participant || m.participant || m.key?.remoteJid
+    const phone = actor.split('@')[0]
+    const groupMetadata = await client.groupMetadata(id).catch(() => null)
+    const groupAdmins = groupMetadata?.participants.filter(p => (p.admin === 'admin' || p.admin === 'superadmin')) || []
+    if (m.messageStubType == 21) {
+      await client.sendMessage(id, { text: `「✎」 @${phone} cambió el nombre del grupo a *${m.messageStubParameters[0]}*`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
+    }
+    if (m.messageStubType == 22) {
+      await client.sendMessage(id, { text: `「✎」 @${phone} cambió el icono del grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
+    }
+    if (m.messageStubType == 23) {
+      await client.sendMessage(id, { text: `「✎」 @${phone} restableció el enlace del grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
+    }
+    if (m.messageStubType == 24) {
+      await client.sendMessage(id, { text: `「✎」 @${phone} cambió la descripción del grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
+    }
+    if (m.messageStubType == 25) {
+      await client.sendMessage(id, { text: `「✎」 @${phone} cambió los ajustes del grupo para permitir que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo.`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
+    }
+    if (m.messageStubType == 26) {
+      await client.sendMessage(id, { text: `「✎」 @${phone} cambió los ajustes del grupo para permitir que ${m.messageStubParameters[0] === 'on' ? 'solo los administradores puedan enviar mensajes al grupo.' : 'todos los miembros puedan enviar mensajes al grupo.'}`, mentions: [actor, ...groupAdmins.map(v => v.id)] })
+    }
+  })
 }
